ebc9b7f8884115f6d3536fc93308c37b5308d6c9
Priyansh Rastogi <priyansh.rastogi54@gmail.com>
2020-04-09 20:25:55 +0530

    added curve analysis

--- src/components/CountryCovidCounter.js
+++ src/components/CountryCovidCounter.js
@@ -4,29 +4,45 @@ import PropTypes from 'prop-types';
 function CountryCovidCounter(props) {
   
   return (
-    <div className="columns is-mobile is-multiline">
-      <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
-        <div style={styles.item} className="has-background-warning">
-          <p className="heading">Total Infected</p>
-          <p className="title is-4">{props.covidData ? props.covidData.confirmed.toLocaleString() : '--'}</p>
+    <div>
+      <div className="columns is-mobile is-multiline">
+        <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
+          <div style={styles.item} className="has-background-warning">
+            <p className="heading">Total Infected</p>
+            <p className="title is-4">{props.covidData ? props.covidData.confirmed.toLocaleString() : '--'}</p>
+          </div>
+        </div>
+        <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
+          <div style={styles.item} className="has-background-danger">
+            <p className="heading">Total Deaths</p>
+            <p className="title is-4">{props.covidData ? props.covidData.deaths.toLocaleString() : '--'}</p>
+          </div>
+        </div>
+        <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
+          <div style={styles.item} className="has-background-success">
+            <p className="heading">Total Recovered</p>
+            <p className="title is-4">{props.covidData ? props.covidData.recovered.toLocaleString() : '--'}</p>
+          </div>
+        </div>
+        <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
+          <div style={styles.item} className="has-background-info">
+            <p className="heading">Total Active</p>
+            <p className="title is-4">{props.covidData ? props.covidData.active.toLocaleString() : '--'}</p>
+          </div>
         </div>
       </div>
-      <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
-        <div style={styles.item} className="has-background-danger">
-          <p className="heading">Total Deaths</p>
-          <p className="title is-4">{props.covidData ? props.covidData.deaths.toLocaleString() : '--'}</p>
+      <div className="columns">
+        <div className="column has-text-centered">
+          <div style={styles.item} className="has-background-grey-dark has-text-white">
+            <p className="heading">Total Infected per Million Population</p>
+            <p className="title is-4 has-text-white">{props.covidData ? props.covidData.infPopRatio.toLocaleString() : '--'}</p>
+          </div>
         </div>
-      </div>
-      <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
-        <div style={styles.item} className="has-background-success">
-          <p className="heading">Total Recovered</p>
-          <p className="title is-4">{props.covidData ? props.covidData.recovered.toLocaleString() : '--'}</p>
-        </div>
-      </div>
-      <div className="column is-half-mobile is-one-quarter-desktop has-text-centered">
-        <div style={styles.item} className="has-background-info">
-          <p className="heading">Total Active</p>
-          <p className="title is-4">{props.covidData ? props.covidData.active.toLocaleString() : '--'}</p>
+        <div className="column has-text-centered">
+          <div style={styles.item} className="has-background-grey-dark has-text-white">
+            <p className="heading">Total Deaths per Million Population</p>
+            <p className="title is-4 has-text-white">{props.covidData ? props.covidData.deathsPopRatio.toLocaleString() : '--'}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/CovidTimeSeriesGraph.js
+++ src/components/CovidTimeSeriesGraph.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {LineChart, BarChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 import *  as d3f from 'd3-format';
 import PropTypes from 'prop-types'; 
@@ -6,9 +6,51 @@ import PropTypes from 'prop-types';
 export default function CovidTimeSeriesGraph(props) {
 
   const [type, setType] = useState('confirmed');
+  const [duration, setDuration] = useState('all');
+  const [timeseries, setTimeseries] = useState(props.covidTimeSeries);
+  const [deltaseries, setDeltaseries] = useState(props.covidTimeSeriesDelta);
+  const [curveAnalysis, setCurveAnalysis] = useState({});
+
+  useEffect(() => {
+    setTimeseries(props.covidTimeSeries);
+    setDeltaseries(props.covidTimeSeriesDelta);
+    analyseCurve(props.covidTimeSeries, props.covidTimeSeriesDelta);
+  }, [props.covidTimeSeries, props.covidTimeSeriesDelta]);
 
   const handleTypeChange = (e) => {
     setType(e.target.value);
+  }
+
+  const handleDurationChange = (d) => {
+    setDuration(d);
+    if(d === 'all') {
+      setTimeseries(props.covidTimeSeries);
+      setDeltaseries(props.covidTimeSeriesDelta);
+    }
+    else if(d === '1m') {
+      setTimeseries(props.covidTimeSeries.slice(Math.max(props.covidTimeSeries.length - 30, 0)));
+      setDeltaseries(props.covidTimeSeriesDelta.slice(Math.max(props.covidTimeSeriesDelta.length - 30, 0)));
+    }
+    else {
+      setTimeseries(props.covidTimeSeries.slice(Math.max(props.covidTimeSeries.length - 15, 0)));
+      setDeltaseries(props.covidTimeSeriesDelta.slice(Math.max(props.covidTimeSeriesDelta.length - 15, 0)));
+    }
+  }
+
+  const analyseCurve = (timeseriesData, deltaseriesData) => {
+    const result = {};
+    if(timeseriesData && Array.isArray(timeseriesData) && timeseriesData.length !== 0) {
+      console.log(timeseriesData);
+      const num7Inf = timeseriesData[timeseriesData.length-1-7].confirmed;
+      const num15Inf = timeseriesData[timeseriesData.length-1-15].confirmed;
+      const num30Inf = timeseriesData[timeseriesData.length-1-30].confirmed;
+      const curInf = timeseriesData[timeseriesData.length-1].confirmed;
+      console.log(num30Inf, num15Inf, num7Inf, curInf);
+      result.ratio7 = (curInf/num7Inf).toFixed(2);
+      result.ratio15 = (curInf/num15Inf).toFixed(2);
+      result.ratio30 = (curInf/num30Inf).toFixed(2);
+    }
+    setCurveAnalysis(result);
   }
 
   const colorMap = {
@@ -36,7 +78,7 @@ export default function CovidTimeSeriesGraph(props) {
       <div className="columns">
         <div className="column">
           <ResponsiveContainer width='100%' height={200}>
-            <LineChart data={props.covidTimeSeries} margin={{top: 0, right: 20, left: 0, bottom: 0}}>
+            <LineChart data={timeseries} margin={{top: 0, right: 20, left: 0, bottom: 0}}>
               <XAxis dataKey="date" tickFormatter={(v) => v.substring(5)}/>
               <YAxis tickFormatter={(v) => d3f.format('.2s')(v)}/>
               <CartesianGrid strokeDasharray="3 3"/>
@@ -47,7 +89,7 @@ export default function CovidTimeSeriesGraph(props) {
         </div>
         <div className="column">
           <ResponsiveContainer width='100%' height={200}>
-            <BarChart data={props.covidTimeSeriesDelta} margin={{top: 0, right: 20, left: 0, bottom: 0}}>
+            <BarChart data={deltaseries} margin={{top: 0, right: 20, left: 0, bottom: 0}}>
               <XAxis dataKey="date" tickFormatter={(v) => v.substring(5)}/>
               <YAxis tickFormatter={(v) => d3f.format('.2s')(v)}/>
               <CartesianGrid strokeDasharray="3 3"/>
@@ -56,6 +98,20 @@ export default function CovidTimeSeriesGraph(props) {
             </BarChart>
           </ResponsiveContainer>
         </div>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'space-between', marginLeft: 20, marginRight: 20}}>
+        <div></div>
+        <div class="buttons has-addons">
+          <button class={`button is-small ${duration === 'all' ? 'is-dark is-selected' : ''}`} onClick={() => handleDurationChange('all')}>All</button>
+          <button class={`button is-small ${duration === '1m' ? 'is-dark is-selected' : ''}`} onClick={() => handleDurationChange('1m')}>1M</button>
+          <button class={`button is-small ${duration === '15d' ? 'is-dark is-selected' : ''}`} onClick={() => handleDurationChange('15d')}>15D</button>
+        </div>
+      </div>
+      <div style={{margin: 20, padding: 20}} className="card notification is-dark is-light">
+        <p className="title is-5">Curve Analysis</p>
+        <p>In last 7 days, number of infected people have increased {curveAnalysis.ratio7} times.</p>
+        <p>In last 15 days, number of infected people have increased {curveAnalysis.ratio15} times.</p>
+        <p>In last 30 days, number of infected people have increased {curveAnalysis.ratio30} times.</p>
       </div>
     </div>
   )
